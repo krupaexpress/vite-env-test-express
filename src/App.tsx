a6e71ad1c45e091f91c729aafd6f695557030d82
Krupa Panchal <krupapanchal.dev@gmail.com>
2024-11-06 11:49:33 +0530

    test

---
+++ src/App.tsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  
+  console.log("VITE_CLIENT_USERNAME = ", import.meta.env.VITE_CLIENT_USERNAME)
+  console.log("REACT_APP_SERVER_USERNAME = ", import.meta.env.REACT_APP_SERVER_USERNAME)
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
